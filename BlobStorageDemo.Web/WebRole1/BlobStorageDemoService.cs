bfc429b6285c9bd116bbc1dc114a189c814b586e
sameersoftyhcl <sameer.softyhcl@hotmail.com>
2018-03-04 18:53:20 +0530

    Updating string

--- BlobStorageDemo.Web/WebRole1/BlobStorageDemoService.cs
+++ BlobStorageDemo.Web/WebRole1/BlobStorageDemoService.cs
@@ -13,7 +13,6 @@ namespace WebRole1
     {
         public CloudBlobContainer GetBlobStrorageDemoContainer()
         {
-            //string connString = "DefaultEndpointsProtocol=https;AccountName=blobstoragedemo;AccountKey=3FS+p8HCPSOPHdcbaxj4ZRYZRG5D1xR4jst0mi5fHoRDvgvav0ld9lDEO5Y8R4mDlkiyKKvcR6CYwAqD6LLNdw==";
             string connString = "";
             string destContainer = "blobcontainerdemo";
 
